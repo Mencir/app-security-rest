34c824135db14ddee9b9618cc1504d4b062212b8
Mencir <smithpalomares4@gmail.com>
2025-08-17 00:19:32 -0500

    inicializacion del proyecto

---
+++ src/main/java/pe/edu/idat/app_security_rest/AppSecurityRestApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_security_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSecurityRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSecurityRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/app_security_rest/AppSecurityRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_security_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSecurityRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
